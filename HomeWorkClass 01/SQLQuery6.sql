5b9b394ccaeb02abd577e183162c87986c1e58df
Todorovski1989 <95313525+Todorovski1989@users.noreply.github.com>
2022-05-31 23:23:13 +0200

    Delete HomeWorkClass 01 directory

--- HomeWorkClass 01/SQLQuery6.sql
+++
@@ -1,9 +0,0 @@
-USE SEDCACADEMYDB
-
-CREATE TABLE [AchievmentType](
-[Id] [int] IDENTITY(1,1) NOT NULL,
-[Name] [nvarchar](100) NULL,
-[Description] [nvarchar](100) NULL,
-[ParticipationRate] [nvarchar](100) NULL
-CONSTRAINT [PK_AchievmentType] PRIMARY KEY CLUSTERED ([Id] ASC)
-)
